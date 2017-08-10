a3d114c722197618c41b054efb7dea3c3516ad6f
ZheniaTrochun <zheniatrochun@ukr.net>
2017-08-10 11:39:44 +0300

    fixed  search by roles

--- client/components/userList/userList.js
+++ client/components/userList/userList.js
@@ -6,12 +6,6 @@ Template.UserList.onCreated(function userListOnCreated() {
   Meteor.subscribe('users');
   this.state = new ReactiveDict();
   this.state.set('nameTemplate', '');
-  // this.state.set('admin', true);
-  // this.state.set('user', true);
-  // this.state.set('blocked', true);
-  Meteor.roles.find({}).fetch().map((r) => r.name).forEach((r) => {
-    this.state.set(r, true);
-  })
   this.state.set('all', true);
 });
 
@@ -24,20 +18,8 @@ Template.UserList.events({
     templateInstance.state.set('all', event.target.checked);
   },
 
-  // 'change #adminRole'(event, templateInstance) {
-  //   templateInstance.state.set('admin', event.target.checked);
-  // },
-  //
-  // 'change #userRole'(event, templateInstance) {
-  //   templateInstance.state.set('user', event.target.checked);
-  // },
-  //
-  // 'change #blockedRole'(event, templateInstance) {
-  //   templateInstance.state.set('blocked', event.target.checked);
-  // }
-
-  'change .role'(event, templateInstance) {
-    templateInstance.state.set(event.target.attributes.id, event.target.cheched);
+  'change .roleCheckbox'(event, templateInstance) {
+    templateInstance.state.set(event.target.attributes.id.value, event.target.checked);
   }
 });
 
@@ -47,12 +29,16 @@ Template.UserList.helpers({
     const users = Meteor.users.find({}).fetch();
     if (users) {
       return users.filter((u) => (u.emails[0].address.indexOf(Template.instance().state.get('nameTemplate')) != -1)
-        && (Template.instance().state.get(getRole(u)) || Template.instance().state.get('all')) );
+        && (u.roles.some((r) => Template.instance().state.get(r)) || Template.instance().state.get('all')) );
     }
   },
 
+  roleState(role) {
+    Template.instance().state.get(role) ? 'checked' : false;
+  },
+
   roles() {
-    return Meteor.roles.find({}).fetch().map((r) => r.name);
+    return Meteor.roles.find({}).map((r) => r.name);
   },
 
   allRoles() {
